d28d6f120a42726fddb3ac47a6be8aeea9475011
Adam Cockell <adamcockell@Adams-MBP.fios-router.home>
2017-12-25 21:22:19 -0500

    less obvious

--- simple.rb
+++ simple.rb
@@ -5,3 +5,22 @@
 # Even in the most simple cases, peer review is likely to introduce insights that we had previously missed
 
 # Here's a basic example
+
+module Tutorial
+
+    ### This method works but is notably suboptimal
+    def get_last_character of_this_string
+        last_character = nil
+        for i in 0..of_this_string.length - 1
+            last_character = of_this_string[i]
+        end
+        return last_character
+    end
+
+    ### This method looks great (especially after seeing the one above), but there's still room for improvement
+    def get_last_character_please of_this_string
+        return of_this_string[of_this_string.length - 1]
+    end
+
+end
+
