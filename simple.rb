1a2190fceaf7b1d7d91516e90338cae2dd655a61
Adam Cockell <adamcockell@Adams-MBP.fios-router.home>
2017-12-25 21:21:23 -0500

    first commit

---
+++ simple.rb
@@ -0,0 +1,7 @@
+# Simple Ruby Module
+
+# The idea is pretty simple:
+# We produce work within the context of our own personal experiences.  
+# Even in the most simple cases, peer review is likely to introduce insights that we had previously missed
+
+# Here's a basic example
